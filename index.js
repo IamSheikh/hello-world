69071b1acc9abd258d81db4221324774c3176caf
IamSheikh <tahajameel.co@gmail.com>
2022-08-30 18:05:19 +0500

    Add support for different languages

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-const helloWorld = () => {
-  return "Hello, World!";
+const { translations } = require("./translations.json");
+
+const helloWorld = (language) => {
+  if (language == "es") {
+    return translations[0].es;
+  } else if (language == "zh") {
+    return translations[0].zh;
+  } else if (language == "fr") {
+    return translations[0].fr;
+  } else if (language == "ja") {
+    return translations[0].ja;
+  } else if (language == "en") {
+    return translations[0].en;
+  } else {
+    return translations[0].en;
+  }
 };
 
 module.exports = helloWorld;
